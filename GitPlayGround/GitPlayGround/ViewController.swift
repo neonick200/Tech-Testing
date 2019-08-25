401da8efe3396f5db7b70dae0147fbe61aaf5c23
tinnakorn pornsontisakul <tinnakorn.pornsontisakul@scb.co.th>
2019-08-25 18:10:28 +0700

    add textlabel and masseage is Hello Git

--- GitPlayGround/GitPlayGround/ViewController.swift
+++ GitPlayGround/GitPlayGround/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var textLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    textLabel.text = "Hello Git"
   }
 
 
