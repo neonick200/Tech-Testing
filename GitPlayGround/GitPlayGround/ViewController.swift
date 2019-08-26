11b665261a6851d799153fd82eb2e131d585e900
tinnakorn pornsontisakul <tinnakorn.pornsontisakul@scb.co.th>
2019-08-26 16:06:40 +0700

    test from daliyprocess branch

--- GitPlayGround/GitPlayGround/ViewController.swift
+++ GitPlayGround/GitPlayGround/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    textLabel.text = "Hi Git"
+    textLabel.text = "Hi Gitfromdaliy"
   }
 
 
